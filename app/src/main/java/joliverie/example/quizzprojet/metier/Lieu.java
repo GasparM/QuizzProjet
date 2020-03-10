468fa7d4aa52fe09ee6515928497eef551288751
gmissiaen <gaspar.missiaen@hotmail.fr>
2020-03-10 16:53:46 +0100

    Mise en place des associations Lieu et Question

--- app/src/main/java/joliverie/example/quizzprojet/metier/Lieu.java
+++ app/src/main/java/joliverie/example/quizzprojet/metier/Lieu.java
@@ -1,13 +1,25 @@
 package joliverie.example.quizzprojet.metier;
 
+import java.util.ArrayList;
+
 public class Lieu {
     protected int id_lieu;
     protected String lieu;
+    protected ArrayList<Question> lesQuestions;
+    protected Lieu leLieu;
 
     public  Lieu (int id_lieu, String lieu){
         this.id_lieu=id_lieu;
         this.lieu=lieu;
     }
+
+    public Lieu getLeLieu() { return leLieu; }
+
+    public void setLeLieu(Lieu leLieu) { this.leLieu = leLieu; }
+
+    public ArrayList<Question> getLesQuestions() {return lesQuestions;}
+
+    public void setLesQuestions(ArrayList<Question> lesQuestions) {this.lesQuestions = lesQuestions;}
 
     public int getId_lieu() {
         return id_lieu;
@@ -24,4 +36,5 @@ public class Lieu {
     public void setLieu(String lieu) {
         this.lieu = lieu;
     }
+
 }
