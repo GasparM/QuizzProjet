6f987b97b270773bed83e0ecbe4946e0711f7f53
Matthias Nicod <matthias.nicod@gmail.com>
2020-03-24 14:20:43 +0100

    Ajouts: + interface carte + interface questions

---
+++ app/src/main/java/joliverie/example/quizzprojet/QuestionActivity.java
@@ -0,0 +1,20 @@
+package joliverie.example.quizzprojet;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class QuestionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_question);
+        getSupportActionBar().hide();
+
+
+    }
+}
